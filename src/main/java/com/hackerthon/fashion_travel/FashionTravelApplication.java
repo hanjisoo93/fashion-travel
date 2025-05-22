631e46403cfa731dc4ed7ba1a35d31edba0a52cd
jisoohan <hanjisoo93@gmail.com>
2025-05-22 14:04:38 +0900

    initial commit

---
+++ src/main/java/com/hackerthon/fashion_travel/FashionTravelApplication.java
@@ -0,0 +1,13 @@
+package com.hackerthon.fashion_travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackerthon/fashion_travel/FashionTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerthon.fashion_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
