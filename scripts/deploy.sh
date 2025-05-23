f2f20bad65241d27fb196f904d53fbcfdc72364b
Jisoo Han <hanjisoo93@gmail.com>
2025-05-23 14:00:25 +0900

    chore: update deploy.sh to run jar directly from build/libs without copy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar | grep -v plain | head -n 1)
+BUILD_DIR=/home/ubuntu/app/build/libs
+DEPLOY_DIR=/home/ubuntu/app
 
+BUILD_JAR=$(ls $BUILD_DIR/*.jar | grep -v plain | head -n 1)
 JAR_NAME=$(basename $BUILD_JAR)
+
 echo ">>> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
 
 echo ">>> build 파일 복사" >> /home/ubuntu/deploy.log
-DEPLOY_PATH=/home/ubuntu/app/
-cp $BUILD_JAR $DEPLOY_PATH
+cp $BUILD_JAR $DEPLOY_DIR/
 
 echo ">>> 현재 실행중인 애플리케이션 pid 확인 후 일괄 종료" >> /home/ubuntu/deploy.log
-sudo ps -ef | grep java | awk '{print $2}' | xargs kill -15
+sudo ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs -r kill -15
 
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo ">>> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
+DEPLOY_JAR=$DEPLOY_DIR/$JAR_NAME
+echo ">>> DEPLOY_JAR 배포" >> /home/ubuntu/deploy.log
 echo ">>> $DEPLOY_JAR의 $JAR_NAME를 실행합니다" >> /home/ubuntu/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2> /home/ubuntu/deploy_err.log &
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>> /home/ubuntu/deploy_err.log &
